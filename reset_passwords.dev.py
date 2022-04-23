46201b9aa22a7f60af4b4f726eb86d62afa2e181
Patrick Felipe <souza.felipe.patrick@gmail.com>
2022-04-23 14:14:27 -0300

    feat: Add project dependency libs

---
+++ reset_passwords.dev.py
@@ -0,0 +1,18 @@
+import os
+import django
+from django.contrib.auth import get_user_model
+
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "souledu.settings")
+django.setup()
+
+User = get_user_model()
+
+DEFAULT_PASSWORD = '123456'
+
+users = User.objects.all()
+
+for user in users:
+    user.set_password(DEFAULT_PASSWORD)
+    user.save()
+
+print(f'Reset passowrds Django: Password: {DEFAULT_PASSWORD}')
